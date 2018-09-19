cbf9eff4a96a872198407bbf2e88b93f19fa2247
martagrrez <martagrrez@gmail.com>
2018-09-19 12:07:23 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@ class User
   field :activation_digest, type: String
   field :activated, type: Boolean, default: false
   field :activated_at, type: DateTime
+  field :reset_digest, type: String
+  field :reset_sent_at, type: DateTime
   
-  attr_accessor :remember_token, :activation_token
+  
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -70,6 +73,22 @@ class User
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+  
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+  
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   
   private
---
+++ db/migrate/20180919095959_add_reset_to_users.rb
@@ -0,0 +1,7 @@
+class AddResetToUsers < Mongoid::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
